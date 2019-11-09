0a418e522d29d71ad80289a5df0bc2603026b70d
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-11-08 21:10:32 -0300

    Adicionado funcionamento nos botões

--- Flutter/Projetos/primeiro_app/lib/main.dart
+++ Flutter/Projetos/primeiro_app/lib/main.dart
@@ -4,43 +4,69 @@ import 'package:flutter/painting.dart';
 void main(){
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,
-        ),
-    ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                child: Text("+1",
-                    style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                child: Text("-1",
-                    style: TextStyle(fontSize: 40.0, color:Colors.white)),
-                onPressed: () {},
-              ),
-            ),
-          ],
-        ),
+    home:Home()));
+}
+class Home extends StatefulWidget {
+  @override
+  _Home_State createState() => _Home_State();
+}
+class _Home_State extends State<Home> {
 
-        Text(
-          "Pode Entrar!",
-          style: TextStyle(
-            color: Colors.white,
-            fontStyle: FontStyle.italic,
-          ),
-        ),
-    ],
-  )));
-} 
+  int _people = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+            children: <Widget>[
+              Image.asset(
+                "imagens/restaurant.jpg",
+                fit: BoxFit.cover,
+                height: 1000.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Pessoas: $_people",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text("+1",
+                              style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                          onPressed: () {
+                           _people++;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text("-1",
+                              style: TextStyle(fontSize: 40.0, color:Colors.white)),
+                          onPressed: () {
+                            _people++;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Text(
+                    "Pode Entrar!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+  }
+}
