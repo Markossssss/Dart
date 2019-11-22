09eddce0c93677b615f793ec415c53973cddea89
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-11-22 20:57:15 -0300

    Layout finalizado

--- Flutter/Projetos/calculadora/lib/main.dart
+++ Flutter/Projetos/calculadora/lib/main.dart
@@ -25,27 +25,44 @@ class _HomeState extends State<Home>{
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person, size: 220.0, color: Colors.green),
-          TextField(keyboardType: TextInputType.number,
+      body: SingleChildScrollView (
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person, size: 220.0, color: Colors.green),
+            TextField(keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: "Peso (Kg)",
-                labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)
+                  labelText: "Peso (Kg)",
+                  labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          TextField(keyboardType: TextInputType.number,
+            TextField(keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: "Altura",
-                labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)
+                  labelText: "Altura",
+                  labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          )
-        ],
+            Container(
+              height: 50.0,
+              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 50.0),
+              child: RaisedButton(
+                color: Colors.green,
+                onPressed: () {},
+                child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 20.0),
+            ),
+          ],
+        ),
       ),
     );
   }
