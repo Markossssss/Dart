9f5df8c4facbb593d6649da1f164512791e36e41
Markossssss <marcospachecoiaw.2018@gmail.com>
2019-11-27 21:26:21 -0300

    Adicionado backend

--- Flutter/Projetos/calculadora/lib/main.dart
+++ Flutter/Projetos/calculadora/lib/main.dart
@@ -12,6 +12,23 @@ class Home extends StatefulWidget {
   _HomeState createState()=>_HomeState();
 }
 class _HomeState extends State<Home>{
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  String _infoText = "Informe seus dados!";
+    void _resetar(){
+      pesoController.text = "";
+      alturaController.text = "";
+      _infoText = "Informe seus dados!";
+    }
+    void _calcular(){
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text)/100;// cm para m
+      double imc = peso / (altura*altura);
+      //calculo do imc peso dividido pela altura ao quadrado.
+        if (imc < 18.6){
+          _infoText = "Abaixo do peso(${imc.toStringAsPrecision(4)})";
+        }
+    }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -21,7 +38,7 @@ class _HomeState extends State<Home>{
         backgroundColor: Colors.green,
         actions: <Widget>[
           IconButton(icon: Icon(Icons.refresh),
-          onPressed: (){},)
+            onPressed: _resetar )
         ],
       ),
       backgroundColor: Colors.white,
@@ -38,6 +55,7 @@ class _HomeState extends State<Home>{
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: pesoController,
             ),
             TextField(keyboardType: TextInputType.number,
               decoration: InputDecoration(
@@ -46,18 +64,23 @@ class _HomeState extends State<Home>{
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: alturaController,
             ),
-            Container(
-              height: 50.0,
-              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 50.0),
-              child: RaisedButton(
-                color: Colors.green,
-                onPressed: () {},
-                child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
+
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  color: Colors.green,
+                  onPressed: _calcular,
+                  child: Text("Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                ),
               ),
             ),
             Text(
-              "Info",
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 20.0),
             ),
